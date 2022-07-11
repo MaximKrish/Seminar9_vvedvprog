76f5aea8853f46409e2cd86053cd117eca4dc594
Maxim <m.krishnevskiy@kmg.kz>
2022-07-11 17:57:02 +0600

    DZ43

---
+++ DZ43/Program.cs
@@ -0,0 +1,35 @@
+﻿// Напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+
+
+double b1, k1, b2, k2, x, y;
+Console.Write("Напишите число b1 = ");
+b1 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Напишите число k1 = ");
+k1 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Напишите число b2 = ");
+b2 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Напишите число k2 = ");
+k2 = Convert.ToDouble(Console.ReadLine());
+x = (b1 - b2) / (k2 - k1);
+y = k1 * x + b1;
+Console.WriteLine("Точка пересечения двух прямых :");
+Console.WriteLine(Convert.ToString(x));
+Console.WriteLine(Convert.ToString(y));
+
+// // Console.WriteLine("Напишите кол-во чисел: ");
+// // n = Convert.ToInt32(Console.ReadLine());
+// // int[] array = new int[n];
+// Console.WriteLine("Напишите числа: ");
+// for (int i = 0; i < array.Length; i++)
+// {
+//     array[i] = Convert.ToInt32(Console.ReadLine());
+//     if(array[i] > 0)
+//     {
+//         count++;
+//     };
+
+// }
+
+
+// Console.WriteLine("Чисел больше 0 :");
+// Console.WriteLine(count);
